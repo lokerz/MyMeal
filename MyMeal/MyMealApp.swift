b1a2b8a077092e3914d3a85a231922ee82e1a144
Ridwan Hanif Abdurrasyid <ridwan.abdurrasyid@yahoo.com>
2023-10-04 16:09:11 +0700

    Initial Commit

---
+++ MyMeal/MyMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMealApp.swift
+//  MyMeal
+//
+//  Created by Ridwan Abdurrasyid on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
